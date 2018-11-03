c908a15bfee63ce6b4539869e4086998c4bf0917
Alkesh Sakhare <akleshsakhare@yahoo.com>
2018-11-03 13:13:09 +0530

    first commit

---
+++ Firefox.js
@@ -0,0 +1,9 @@
+describe('angularjs homepage todo list', function() {
+  it('should add a todo', function() {
+	
+	browser.manage().window().maximize();  
+	  
+    browser.get('https://angularjs.org');
+
+  
+  }}
