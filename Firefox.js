a0aa665c51944f9c0e778fb2cee0543aa7582959
unknown <akleshsakhare@yahoo.com>
2020-06-07 10:04:47 +0530

    update protractor

--- Firefox.js
+++
@@ -1,9 +0,0 @@
-describe('angularjs homepage todo list', function() {
-  it('should add a todo', function() {
-	
-	browser.manage().window().maximize();  
-	  
-    browser.get('https://angularjs.org');
-
-  
-  }}
